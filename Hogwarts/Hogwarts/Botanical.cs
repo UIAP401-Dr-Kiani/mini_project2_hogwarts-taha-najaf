58396de475fb9f7cd9a1e85b6a0e55f4fadd433f
taha-najaf <127234497+taha-najaf@users.noreply.github.com>
2023-04-25 00:04:45 +0330

    Botanical class add

---
+++ Hogwarts/Hogwarts/Botanical.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hogwarts
+{
+    public class Botanical : Lessons
+    {
+        public List<string> ListOfFirstSemesterplants { get; set; }
+        public List<string> ListOfSecondSemesterplants { get; set; }
+        public List<string> ListOfThirdSemesterplants { get; set; }
+        public List<string> ListOfFourthSemesterplants { get; set; }
+    }
+}

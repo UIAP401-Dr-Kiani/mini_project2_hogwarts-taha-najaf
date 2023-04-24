a7b417bb138f2bcb8e8ef6de6a37e096ebad682e
taha-najaf <127234497+taha-najaf@users.noreply.github.com>
2023-04-25 00:09:17 +0330

    Occultism class add

---
+++ Hogwarts/Hogwarts/Occultism.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hogwarts
+{
+    public class Occultism : Lessons
+    {
+        public List<string> words { get; set; } 
+    }
+}

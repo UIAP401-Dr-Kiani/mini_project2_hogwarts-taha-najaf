4d9340feb65e5c07c70cb3de11dd8a24bd3d61f6
taha-najaf <127234497+taha-najaf@users.noreply.github.com>
2023-04-24 23:34:09 +0330

    Student class add

---
+++ Hogwarts/Hogwarts/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hogwarts
+{
+    public class Student
+    {
+        public int PassedUnits { get; set; }
+        public int Term { get; set; }
+        public int HostelNumber { get; set; }
+
+     }
+}

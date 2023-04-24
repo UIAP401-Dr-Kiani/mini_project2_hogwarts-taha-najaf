7efa268d23a9ccff632cdc246c4f5e95535edb15
taha-najaf <127234497+taha-najaf@users.noreply.github.com>
2023-04-24 23:17:55 +0330

    person class create

---
+++ Hogwarts/Hogwarts/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hogwarts
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Birthday { get; set; }
+        public string Gender { get; set; }
+        public string FatherName { get; set; }
+        public int Username { get; set;}
+        public int Password { get; set; }
+        public BreedType breedType { get ; set; } 
+    }
+}

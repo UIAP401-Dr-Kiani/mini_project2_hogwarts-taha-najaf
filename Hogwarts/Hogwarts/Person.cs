72b0b9085926edc20646d33aa4e9b56a92090a96
taha-najaf <127234497+taha-najaf@users.noreply.github.com>
2023-04-25 16:07:44 +0330

    read data from file

--- Hogwarts/Hogwarts/Person.cs
+++ Hogwarts/Hogwarts/Person.cs
@@ -10,11 +10,11 @@ namespace Hogwarts
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public int Birthday { get; set; }
+        public string Birthday { get; set; }
         public string Gender { get; set; }
         public string FatherName { get; set; }
-        public int Username { get; set;}
-        public int Password { get; set; }
+        public string Username { get; set;}
+        public string Password { get; set; }
         public BreedType breedType { get ; set; } 
     }
 }
--- Hogwarts/Hogwarts/Program.cs
+++ Hogwarts/Hogwarts/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,38 @@ namespace Hogwarts
     {
         static void Main(string[] args)
         {
+            List <AllowedPersons> persons = new List <AllowedPersons> ();
+            using (StreamReader file = new StreamReader("file.tsv"))
+            {
+                string ln;
+
+                while ((ln = file.ReadLine()) != null)
+                {
+                    AllowedPersons allowedPersons = new AllowedPersons();
+                    string[] human = ln.Split('\t').ToArray<string>();
+
+                    allowedPersons.FirstName = human[0];
+                    allowedPersons.LastName = human[1];
+                    allowedPersons.Birthday = human[2];
+                    allowedPersons.Gender = human[3];
+                    allowedPersons.FatherName = human[4];
+                    allowedPersons.Username = human[5];
+                    allowedPersons.Password = human[6];
+                    if (human[7] == "HalfBlood")// for breed or race type
+                        allowedPersons.breedType = BreedType.HalfBlood;
+                    else if (human[7] == "PureBlood")
+                        allowedPersons.breedType = BreedType.PureBlood;
+                    else if (human[7] == "MuggleBlood")
+                        allowedPersons.breedType = BreedType.MuggleBlood;
+                    if (human[8] == "teacher")//for role
+                        allowedPersons.Role = Role.teacher;
+                    else if (human[8] == "student")
+                        allowedPersons.Role = Role.student;
+
+                    persons.Add(allowedPersons);
+                }
+                file.Close();
+            }
         }
     }
 }

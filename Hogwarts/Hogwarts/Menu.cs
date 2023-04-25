c9e873d0d2a0d3169a9c6cf5d9b622f9d9f953e8
taha-najaf <127234497+taha-najaf@users.noreply.github.com>
2023-04-25 17:03:08 +0330

    Teacher and Student menu add

--- Hogwarts/Hogwarts/Menu.cs
+++ Hogwarts/Hogwarts/Menu.cs
@@ -23,13 +23,37 @@ namespace Hogwarts
             Console.ForegroundColor= ConsoleColor.Yellow;
             Console.WriteLine("     ----****Welcome to the Dumbledore(Admin) menu****----       ");
             Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("Please enter your choice by numbers:     ");
+            Console.WriteLine("Please enter your choice by number:     ");
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("1=>Sending letters to students\t2=>Exit");
             int choice=int.Parse(Console.ReadLine());
             Console.ResetColor();
             return choice;
         }
-        
+        public static int TeacherMenu()
+        {
+            Console.ForegroundColor=ConsoleColor.Yellow;
+            Console.WriteLine("     ----****Welcome to the Teachers menu****----       ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Please enter your choice by numbers:     ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("1=>Define lessons\t2=>Define homework\t3=>Set score for students\t4=>Confirm students grades\t5=>Exit  ");
+            int choice = int.Parse(Console.ReadLine());
+            Console.ResetColor();
+            return choice;
+        }
+        public static int StudentMenu()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("     ----****Welcome to the Students menu****----       ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Please enter your choice by numbers:     ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("1=>Send request to Dumbledore for going back to the city\t2=>Take your data\t3=>Choosing unit\t" +
+                "4=>Upload homework\t5=>Exit");
+            Console.ResetColor();
+            int choice = int.Parse(Console.ReadLine());
+            return choice;
+        }
     }
 }

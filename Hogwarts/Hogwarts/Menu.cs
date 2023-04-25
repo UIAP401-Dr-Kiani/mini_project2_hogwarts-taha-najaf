a5dbb1069dd6cc72d8fb8b315924a7b42b3be131
taha-najaf <127234497+taha-najaf@users.noreply.github.com>
2023-04-25 16:48:18 +0330

    dumbledore menu add

--- Hogwarts/Hogwarts/Menu.cs
+++ Hogwarts/Hogwarts/Menu.cs
@@ -8,15 +8,28 @@ namespace Hogwarts
 {
     public class Menu
     {
-       public static int EnteryMenu()
+        public static int EnteryMenu()
         {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("     -----*****Welcome to the Hogwarts Management System****----     ");
-            Console.ForegroundColor= ConsoleColor.Blue;
-            Console.WriteLine("Please enter your role by number\n 1=>Dumbledore\t2=>Teachers\t3=>Student\t4=>Exit");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Please enter your role by number:\n 1=>Dumbledore\t2=>Teachers\t3=>Student\t4=>Exit");
+            int choice = int.Parse(Console.ReadLine());
+            Console.ResetColor();
+            return choice;
+        }
+        public static int DumbledoreMenu()
+        {
+            Console.ForegroundColor= ConsoleColor.Yellow;
+            Console.WriteLine("     ----****Welcome to the Dumbledore(Admin) menu****----       ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Please enter your choice by numbers:     ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("1=>Sending letters to students\t2=>Exit");
             int choice=int.Parse(Console.ReadLine());
             Console.ResetColor();
             return choice;
         }
+        
     }
 }

b236e096bd3659bea8c8f1a165f8f91fb8c70866
taha-najaf <127234497+taha-najaf@users.noreply.github.com>
2023-05-04 00:33:50 +0330

    method of defining lesson add

--- Hogwarts/Hogwarts/Lessons.cs
+++ Hogwarts/Hogwarts/Lessons.cs
@@ -13,5 +13,18 @@ namespace Hogwarts
         public int Capacity { get; set; }
         public int CurrentTerm { get; set; }
         public string Name { get; set; }
+        public  void DefineLesson(string name, string time, int capacity, int currentTerm)
+        {
+            Name = name;
+            Time = time;
+            Capacity = capacity;
+            CurrentTerm = currentTerm;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"The lesson: {name}\n" +
+                $" with time:{time}\n" +
+                $"capacity: {capacity}\n" +
+                $"in term {currentTerm} defined");
+            Console.ResetColor();
+        }
     }
 }
